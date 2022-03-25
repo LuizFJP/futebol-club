d836d469e1cb2244ee73e3b938b34a00db533f54
LuizFJP <luizfjportela@gmail.com>
2022-03-25 20:50:11 -0300

    requirement creating getbyid in controller

--- app/backend/src/controllers/clubsController.ts
+++ app/backend/src/controllers/clubsController.ts
@@ -8,6 +8,12 @@ class Clubs {
     const clubs = await clubsService.getAllClub();
     return res.status(200).json(clubs);
   }
+
+  public static async getById(req: Request, res: Response) {
+    const { id } = req.params;
+    const club = await clubsService.getByIdService(JSON.parse(id));
+    return res.status(200).json(club);
+  }
 }
 
 export default Clubs;
