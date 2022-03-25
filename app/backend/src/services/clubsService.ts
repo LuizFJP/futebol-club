72434ec162e3cd7de84860101223feb12b5da303
LuizFJP <luizfjportela@gmail.com>
2022-03-25 20:47:07 -0300

    requirement creating getbyid in service

--- app/backend/src/services/clubsService.ts
+++ app/backend/src/services/clubsService.ts
@@ -7,6 +7,11 @@ class Clubs {
     const clubs = await this._clubsModel.getAllModel();
     return clubs;
   }
+
+  public async getByIdService(id: number) {
+    const club = await this._clubsModel.getByIdModel(id);
+    return club;
+  }
 }
 
 export default new Clubs();
