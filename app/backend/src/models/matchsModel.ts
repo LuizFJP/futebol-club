a3ce8eabc2d2aa63b03c6bb8792fcc2cd880051a
LuizFJP <luizfjportela@gmail.com>
2022-03-27 15:34:43 -0300

    fix: add match route

--- app/backend/src/models/matchsModel.ts
+++ app/backend/src/models/matchsModel.ts
@@ -1,10 +1,27 @@
+import { Op, Sequelize } from 'sequelize';
+import Club from '../database/models/Clubs';
 import Match from '../database/models/Matchs';
 
 class MatchsModel {
   private _Match = Match;
 
+  private _true = true;
+
+  private _false = false;
+
   public async getAllMatchsModel() {
-    const matchs = await this._Match.findAll({ raw: true });
+    const matchs = await this._Match.findAll({
+      nest: true,
+      include: [{ model: Club,
+        as: 'homeClub',
+        attributes: { exclude: ['id'] },
+      },
+      {
+        model: Club,
+        as: 'awayClub',
+        attributes: { exclude: ['id'] },
+      }],
+    });
     return matchs;
   }
 }
