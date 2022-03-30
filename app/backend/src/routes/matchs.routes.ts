ebff1c33e5c38df4432fa27b044f92b3dd85fe48
LuizFJP <luizfjportela@gmail.com>
2022-03-29 23:38:47 -0300

    feature: add update inProgress route

--- app/backend/src/routes/matchs.routes.ts
+++ app/backend/src/routes/matchs.routes.ts
@@ -5,5 +5,6 @@ const route = Router();
 
 route.get('/', matchsController.getAllMatchs);
 route.post('/', matchsController.createMatch);
+route.put('/:id/finish', matchsController.updateInProgressFalse);
 
 export default route;
